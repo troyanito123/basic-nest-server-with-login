65b0e4fb8fe234570732aacfafeb51fbf4ce151f
jassmanni <jassmanniq@gmail.com>
2021-02-27 10:00:40 -0400

    config initial. database with migrations. Validations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { config } from './config/config';
+import { DatabaseConfig } from './config/database.config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ConfigOptions } from './config/config';
+import generateTypeormConfigFile from './scripts/generate-typeormconfig';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  // generate ormconfig.json
+  generateTypeormConfigFile(configService);
+  const port = configService.get(ConfigOptions.port);
+  await app.listen(port);
 }
 bootstrap();
